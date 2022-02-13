0a1c36694da27c62fe0ca8811db60a8befbc9864
Irina <golubkovairina76@gmail.com>
2022-02-13 22:43:36 +0300

    Change of code and name

---
+++ src/main/java/SpeedPlayersMethods.java
@@ -0,0 +1,74 @@
+public class SpeedPlayersMethods {
+    public static boolean isGreenLight = false;
+
+    public static int numberOfDropouts(int[] speedOfPlayer) {
+        int countOfPlayers = 0;
+        for (int i = 0; i != speedOfPlayer.length; ++i) {
+
+            if (isPlayerDroppedOut(speedOfPlayer[i])) {
+                ++countOfPlayers;
+
+            }
+
+        }
+        return countOfPlayers;
+    }
+    public static boolean isPlayerDroppedOut(int speedOfPlayer) {
+        //return isGreenLight == false && speedOfPlayer != 0;
+        if (isGreenLight == false) {
+            if (speedOfPlayer != 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+
+    public static int[] speedsOfDropped(int[] speedOfPlayer) {
+
+        int cnt = numberOfDropouts(speedOfPlayer);
+
+        int[] result = new int[cnt];
+        int resultCount = 0;
+        for (int i = 0; i != speedOfPlayer.length; ++i) {
+
+            if (isPlayerDroppedOut(speedOfPlayer[i])) {
+                result[resultCount] = speedOfPlayer[i];
+                ++resultCount;
+
+
+            }
+
+        }
+        return result;
+    }
+
+
+    public static int[] speedsOfNotDropped(int[] speedOfPlayer) {
+
+        int cnt = speedOfPlayer.length - numberOfDropouts(speedOfPlayer);
+
+        int[] result = new int[cnt];
+        int resultCount = 0;
+        for (int i = 0; i != speedOfPlayer.length; ++i) {
+
+            if (isPlayerDroppedOut(speedOfPlayer[i]) == false) {
+                result[resultCount] = speedOfPlayer[i];
+                ++resultCount;
+
+
+            }
+
+        }
+        return result;
+    }
+
+
+
+}
+
+
+
---
+++ src/test/java/SpeedPlayersMethodsTest.java
@@ -0,0 +1,157 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SpeedPlayersMethodsTest {
+
+    @Test
+    public void numberOfDropoutsWhenLightIsRed() {
+        SpeedPlayersMethods.isGreenLight = false;
+
+        int[] speedOfPlayer = {0, 3, 2,};
+        int expected = 2;
+
+        int actual = SpeedPlayersMethods.numberOfDropouts(speedOfPlayer);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void numberOfDropoutsWhenLightIsGreen() {
+        SpeedPlayersMethods.isGreenLight = true;
+
+        int[] speedOfPlayer = {0, 3, 2,};
+        int expected = 0;
+
+        int actual = SpeedPlayersMethods.numberOfDropouts(speedOfPlayer);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void numberOfDropoutsWithGreenColorNegativeSpeed() {
+        SpeedPlayersMethods.isGreenLight = false;
+
+        int[] speedOfPlayer = {0, -3, 2,};
+        int expected = 2;
+
+        int actual = SpeedPlayersMethods.numberOfDropouts(speedOfPlayer);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void numberOfDropoutsWhenColorIsRedNegativeSpeed() {
+        SpeedPlayersMethods.isGreenLight = true;
+
+        int[] speedOfPlayer = {0, -3, 2,};
+
+        int expected = 0;
+        int actual = SpeedPlayersMethods.numberOfDropouts(speedOfPlayer);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void SpeedOfDropoutsAtRed() {
+        int[] speedOfPlayer = {0, 3, 2,};
+        SpeedPlayersMethods.isGreenLight = false;
+
+        int[] expected = {3, 2};
+        int[] actual = SpeedPlayersMethods.speedsOfDropped(speedOfPlayer);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void SpeedOfDropoutsWhenGreen() {
+        int[] speedOfPlayer = {0, 3, 2,};
+        SpeedPlayersMethods.isGreenLight = true;
+
+        int[] expected = {};
+        int[] actual = SpeedPlayersMethods.speedsOfDropped(speedOfPlayer);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void SpeedOfDropoutsInRedIsNegativeSpeed() {
+        int[] speedOfPlayer = {0, 3, -2,};
+        SpeedPlayersMethods.isGreenLight = false;
+
+        int[] expected = {3, -2};
+        int[] actual = SpeedPlayersMethods.speedsOfDropped(speedOfPlayer);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void SpeedOfDropoutsInGreenIsNegativeSpeed() {
+        int[] speedOfPlayer = {0, 3, -2,};
+        SpeedPlayersMethods.isGreenLight = true;
+
+        int[] expected = {};
+        int[] actual = SpeedPlayersMethods.speedsOfDropped(speedOfPlayer);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void SpeedOfNonEliminatorsAtRed() {
+        int[] speedOfPlayer = {0, 3, 2,};
+        SpeedPlayersMethods.isGreenLight = false;
+
+        int[] expected = {0};
+        int[] actual = SpeedPlayersMethods.speedsOfNotDropped(speedOfPlayer);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void SpeedOfNonEliminatorsWhenGreen() {
+        int[] speedOfPlayer = {0, 3, 2,};
+        SpeedPlayersMethods.isGreenLight = true;
+
+        int[] expected = {0, 3, 2};
+        int[] actual = SpeedPlayersMethods.speedsOfNotDropped(speedOfPlayer);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void SpeedOfNonEliminatorsWithRedColorAndNegativeSpeed() {
+        int[] speedOfPlayer = {0, 3, -2,};
+        SpeedPlayersMethods.isGreenLight = false;
+
+        int[] expected = {0};
+        int[] actual = SpeedPlayersMethods.speedsOfNotDropped(speedOfPlayer);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void SpeedOfNonDropoutsWithGreenColorAndNegativeSpeed() {
+        int[] speedOfPlayer = {0, 3, -2,};
+        SpeedPlayersMethods.isGreenLight = true;
+
+        int[] expected = {0, 3, -2};
+        int[] actual = SpeedPlayersMethods.speedsOfNotDropped(speedOfPlayer);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+}
+
