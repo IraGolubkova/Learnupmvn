aa14143167221be2d8e5f4aeb0c1d53c54247387
Irina <golubkovairina76@gmail.com>
2022-02-25 18:14:16 +0300

    Adding homework

---
+++ src/test/java/Homework20/TestsForTheNonStaticGameMethodTest.java
@@ -0,0 +1,128 @@
+package Homework20;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestsForTheNonStaticGameMethodTest {
+
+    @Test
+    public void ArrayOfNamesOfSurvivingPlayersIsRed() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(false, 0);
+
+        String[] names = {"Elena 0", "Victor 3", "Maria 2",};
+        String[] expected = {"Elena"};
+        String[] actual = NonStaticGameClassTask20.namesOfSurvivorsNonStatic(names);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void ArrayOfNamesOfSurvivingPlayersIsGreen() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(true, 0);
+
+        String[] names = {"Elena 0", "Victor 3", "Maria 2",};
+        String[] expected = {};
+        String[] actual = NonStaticGameClassTask20.namesOfSurvivorsNonStatic(names);
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void ArrayOfNamesOfSurvivingPlayersInRedNegativeSpeed() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(false, 0);
+
+        String[] names = {"Elena 0", "Victor -3", "Maria 2",};
+        String[] expected = {"Elena"};
+        String[] actual = NonStaticGameClassTask20.namesOfSurvivorsNonStatic(names);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ArrayOfNamesOfSurvivingPlayersInGreenNegativeSpeed() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(true, 0);
+
+        String[] names = {"Elena 0", "Victor -3", "Maria 2",};
+        String[] expected = {};
+        String[] actual = NonStaticGameClassTask20.namesOfSurvivorsNonStatic(names);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ArrayOfNamesOfSurvivingPlayersRedMaxSpeed2() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(false, 2);
+
+        String[] names = {"Elena 0", "Victor 3", "Maria 2",};
+        String[] expected = {"Elena", "Maria"};
+        String[] actual = NonStaticGameClassTask20.namesOfSurvivorsNonStatic(names);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ArrayOfNamesOfSurvivingPlayersGreenMaxSpeed2() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(true, 2);
+
+        String[] names = {"Elena 0", "Victor 3", "Maria 2",};
+        String[] expected = {};
+        String[] actual = NonStaticGameClassTask20.namesOfSurvivorsNonStatic(names);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ArrayOfNamesOfSurvivingPlayersRedMaxSpeed5() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(false, 5);
+
+        String[] names = {"Elena 0", "Victor 3", "Maria 2",};
+        String[] expected = {"Elena", "Victor", "Maria"};
+        String[] actual = NonStaticGameClassTask20.namesOfSurvivorsNonStatic(names);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ArrayOfNamesOfSurvivingPlayersGreenMaxSpeed5() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(true, 5);
+
+        String[] names = {"Elena 0", "Victor 3", "Maria 2",};
+        String[] expected = {};
+        String[] actual = NonStaticGameClassTask20.namesOfSurvivorsNonStatic(names);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ArrayOfNamesOfSurvivingPlayersInRedNegative2Speed() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(false, 0);
+
+        String[] names = {"Elena 0", "Victor -3", "Maria -2",};
+        String[] expected = {"Elena"};
+        String[] actual = NonStaticGameClassTask20.namesOfSurvivorsNonStatic(names);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ArrayOfNamesOfSurvivingPlayersInGreenNegative2Speed() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(true, 0);
+
+        String[] names = {"Elena 0", "Victor -3", "Maria -2",};
+        String[] expected = {};
+        String[] actual = NonStaticGameClassTask20.namesOfSurvivorsNonStatic(names);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+}
+
+
---
+++ src/test/java/Homework20/TestsForTheNonStaticGameMethods2Test.java
@@ -0,0 +1,167 @@
+package Homework20;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestsForTheNonStaticGameMethods2Test {
+
+    @Test
+    public void numberOfDropoutsWhenLightIsRed() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(false, 2);
+
+        int[] speedOfPlayer = {0, 3, 2};
+
+        int expected = 1;
+        int actual = NonStaticGameClassTask20.numberOfDropouts(speedOfPlayer);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void numberOfDropoutsWhenLightIsGreen() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(true, 2);
+
+        int[] speedOfPlayer = {0, 3, 2};
+
+        int expected = 0;
+        int actual = NonStaticGameClassTask20.numberOfDropouts(speedOfPlayer);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void numberOfDropoutsWithGreenColorNegativeSpeed() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(false, 2);
+
+        int[] speedOfPlayer = {0, -3, 2};
+
+        int expected = 1;
+        int actual = NonStaticGameClassTask20.numberOfDropouts(speedOfPlayer);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void numberOfDropoutsWhenColorIsRedNegativeSpeed() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(true, 2);
+
+        int[] speedOfPlayer = {0, -3, 2};
+
+        int expected = 0;
+        int actual = NonStaticGameClassTask20.numberOfDropouts(speedOfPlayer);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void speedOfDropoutsAtRed() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(false, 2);
+
+        int[] speedOfPlayer = {0, 3, 2};
+
+        int[] expected = {3};
+        int[] actual = NonStaticGameClassTask20.speedsOfDropped(speedOfPlayer);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void speedOfDropoutsWhenGreen() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(true, 2);
+
+        int[] speedOfPlayer = {0, 3, 2};
+
+        int[] expected = {};
+        int[] actual = NonStaticGameClassTask20.speedsOfDropped(speedOfPlayer);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void speedOfDropoutsInRedIsNegativeSpeed() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(false, 2);
+
+        int[] speedOfPlayer = {0, 3, -2,};
+
+        int[] expected = {3};
+        int[] actual = NonStaticGameClassTask20.speedsOfDropped(speedOfPlayer);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void speedOfDropoutsInGreenIsNegativeSpeed() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(true, 2);
+
+        int[] speedOfPlayer = {0, 3, -2};
+
+        int[] expected = {};
+        int[] actual = NonStaticGameClassTask20.speedsOfDropped(speedOfPlayer);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void speedOfNonEliminatorsAtRed() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(false, 2);
+
+        int[] speedOfPlayer = {0, 3, 2};
+
+        int[] expected = {0, 2};
+        int[] actual = NonStaticGameClassTask20.speedsOfNotDropped(speedOfPlayer);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void speedOfNonEliminatorsWhenGreen() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(true, 2);
+
+        int[] speedOfPlayer = {0, 3, 2};
+
+        int[] expected = {0, 3, 2};
+        int[] actual = NonStaticGameClassTask20.speedsOfNotDropped(speedOfPlayer);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void speedOfNonEliminatorsWithRedColorAndNegativeSpeed() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(false, 2);
+
+        int[] speedOfPlayer = {0, 3, -2};
+
+        int[] expected = {0, -2};
+        int[] actual = NonStaticGameClassTask20.speedsOfNotDropped(speedOfPlayer);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void speedOfNonDropoutsWithGreenColorAndNegativeSpeed() {
+        NonStaticGameClassTask20 nonStaticGameClassTask20 = new NonStaticGameClassTask20(true, 2);
+
+        int[] speedOfPlayer = {0, 3, -2};
+
+        int[] expected = {0, 3, -2};
+        int[] actual = NonStaticGameClassTask20.speedsOfNotDropped(speedOfPlayer);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+
+}
